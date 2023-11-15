c95f0b7757c90f93c06551a90df419cc143079d4
Maycee Nelson <Mayceenelson@yahoo.com>
2023-11-15 13:48:02 -0700

    new_commit

---
+++ ending.py
@@ -0,0 +1,12 @@
+def get_initials(name):
+     
+    name_string = ""
+
+    name_list = name.split()
+    for item in name_list:
+        first = item[0].upper()
+        name_string += first
+
+    return name_string
+
+          
